e8f93c68b6212ffa826992d939cc18664891e520
Luiz Felipe <52084917+LuizFelipee96@users.noreply.github.com>
2023-12-18 08:20:09 -0300

    add past

--- back/index.js
+++ back/index.js
@@ -7,18 +7,19 @@ const port = 3000;
 
 const upload = multer({ dest: 'uploads/' });
 
+
 app.post('/processarLivro', upload.single('livro'), async (req, res) => {
     if (!req.file) {
         return res.status(400).send('Nenhum arquivo foi enviado.');
     }
 
     const tamanhoResumo = parseInt(req.body.tamanhoResumo) || 1;
+    console.log("Tamanho do Resumo Recebido:", tamanhoResumo);
 
     try {
         const dataBuffer = await fs.readFile(req.file.path);
         const pdfData = await pdfParse(dataBuffer);
 
-        // Dividir o texto por páginas (aproximadamente)
         const paginas = pdfData.text.split(/\f/);
         const textoResumo = paginas.slice(0, tamanhoResumo).join('\n\n');
 
@@ -27,6 +28,7 @@ app.post('/processarLivro', upload.single('livro'), async (req, res) => {
         res.status(500).send('Erro ao processar o livro: ' + error.message);
     }
 });
+
 
 app.listen(port, () => {
     console.log(`Servidor rodando na porta ${port}`);
